12c1c25d7b126a9d1fa0ce7d4063aef7b61b1410
aristovz <pavelaristov@MacBook-Air-Pavel.local>
2017-02-22 06:36:03 +0300

    final version

---
+++ TinkoffApp/API.swift
@@ -0,0 +1,123 @@
+//
+//  API.swift
+//  TinkoffApp
+//
+//  Created by Pavel Aristov on 22.02.17.
+//  Copyright © 2017 aristovz. All rights reserved.
+//
+
+import Foundation
+
+class API {
+    //MARK: - getListOfCurrency()
+    static open func getListOfCurrency(completion: @escaping (Array<String>, String?) -> Void) {
+        self.requestListCurrency() { (data, error) in
+            var _error: String? = nil
+            var currencies = Array<String>()
+            
+            if let currentError = error {
+                _error = currentError.localizedDescription
+            } else {
+                self.parseListCurrencyResponse(data: data, completion: { (value, error) in
+                    if let parseError = error {
+                        _error = parseError
+                    }
+                    else {
+                        currencies = value
+                    }
+                })
+            }
+            
+            completion(currencies, _error)
+        }
+    }
+    
+    fileprivate static func parseListCurrencyResponse(data: Data?, completion: @escaping (Array<String>, String?) -> Void) {
+        var value = Array<String>()
+        var _error: String? = nil
+        
+        do {
+            let json = try JSONSerialization.jsonObject(with: data!, options: []) as? Dictionary<String, Any>
+            
+            if let parsedJSON = json {
+                if let rates = parsedJSON["rates"] as? Dictionary<String, Double> {
+                    if let base = parsedJSON["base"] as? String {
+                        value.append(base)
+                    }
+                    
+                    for (rate, _) in rates {
+                        value.append(rate)
+                    }
+                    completion(value, nil)
+                } else {
+                    _error = "No \"rates\" field found"
+                }
+            } else {
+                _error = "No JSON value parsed"
+            }
+        } catch { _error = error.localizedDescription }
+        
+        completion(value, _error)
+    }
+    
+    fileprivate static func requestListCurrency(parseHandler: @escaping (Data?, Error?) -> Void) {
+        let url = URL(string: "https://api.fixer.io/latest")
+        
+        let dataTask = URLSession.shared.dataTask(with: url!) {
+            (dataReceived, response, error) in
+            parseHandler(dataReceived, error)
+        }
+        
+        dataTask.resume()
+    }
+
+    //MARK: - getCurrencyRates()
+    static open func getCurrencyRate(baseCurrency: String, toCurrency: String, completion: @escaping (String) -> Void) {
+        self.requestCurrencyRates(baseCurrency: baseCurrency) { (data, error) in
+            var string = "No currency retrieved!"
+            
+            if let currentError = error {
+                string = currentError.localizedDescription
+            } else {
+                string = self.parseCurrencyRatesResponse(data: data, toCurrency: toCurrency)
+            }
+            
+            completion(string)
+        }
+    }
+    
+    fileprivate static func parseCurrencyRatesResponse(data: Data?, toCurrency: String) -> String {
+        var value = ""
+        
+        do {
+            let json = try JSONSerialization.jsonObject(with: data!, options: []) as? Dictionary<String, Any>
+            
+            if let parsedJSON = json {
+                if let rates = parsedJSON["rates"] as? Dictionary<String, Double> {
+                    if let rate = rates[toCurrency] {
+                        value = "\(rate)"
+                    } else {
+                        value = "No rate for currency \"\(toCurrency)\" found"
+                    }
+                } else {
+                    value = "No \"rates\" field found"
+                }
+            } else {
+                value = "No JSON value parsed"
+            }
+        } catch { value = error.localizedDescription }
+        
+        return value
+    }
+    
+    fileprivate static func requestCurrencyRates(baseCurrency: String, parseHandler: @escaping (Data?, Error?) -> Void) {
+        let url = URL(string: "https://api.fixer.io/latest?base=\(baseCurrency)")
+        
+        let dataTask = URLSession.shared.dataTask(with: url!) {
+            (dataReceived, response, error) in
+            parseHandler(dataReceived, error)
+        }
+        
+        dataTask.resume()
+    }
+}
--- TinkoffApp/ViewController.swift
+++ TinkoffApp/ViewController.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet weak var fromCurrencyLabel: UILabel!
-    @IBOutlet weak var toCurrencyLabel: UILabel!
+    @IBOutlet weak var fromCurrencyField: UITextField!
+    @IBOutlet weak var toCurrencyField: UITextField!
     
     @IBOutlet weak var fromValueField: UITextField!
     @IBOutlet weak var toValueLabel: UILabel!
@@ -20,15 +20,119 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    var currencies = Array<String>()
+    var currenciesExceptBase: Array<String> {
+        get {
+            var _currenciesExceptBase = currencies
+            _currenciesExceptBase.remove(at: fromPickerView.selectedRow(inComponent: 0))
+            return _currenciesExceptBase
+        }
+    }
+    
+    var currentRate = 1.0
+    
+    var fromPickerView = UIPickerView()
+    var toPickerView = UIPickerView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1)
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.done, target: self, action: #selector(donePicker))
+        
+        toolBar.setItems([doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        
+        fromPickerView.delegate = self
+        fromPickerView.showsSelectionIndicator = true
+        fromCurrencyField.inputView = fromPickerView
+        fromCurrencyField.inputAccessoryView = toolBar
+        
+        toPickerView.delegate = self
+        toPickerView.showsSelectionIndicator = true
+        toCurrencyField.inputView = toPickerView
+        toCurrencyField.inputAccessoryView = toolBar
+        
+        API.getListOfCurrency { (value, error) in
+            if let currentError = error {
+                print(currentError)
+            } else {
+                self.currencies = value
+                
+                DispatchQueue.main.async(execute: {
+                    if self.currencies.count > 1 {
+                        self.fromPickerView.reloadAllComponents()
+                        self.toPickerView.reloadAllComponents()
+                        
+                        self.pickerView(self.fromPickerView, didSelectRow: 0, inComponent: 0)
+                        self.pickerView(self.toPickerView, didSelectRow: 0, inComponent: 0)
+                    }
+                })
+            }
+        }
     }
 
-    @IBAction func changeCurrencyButtonAction(_ sender: UIButton) {
-        
+    func donePicker() {
+        self.view.endEditing(true)
     }
     
+    @IBAction func changeFromValue(_ sender: UITextField) {
+        if let inputValue = Double(fromValueField.text!.replacingOccurrences(of: ",", with: ".")) {
+            toValueLabel.text = "\(inputValue * currentRate)"
+        }
+    }
     
+    //MARK: - UITextFieldDelegate
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return false
+    }
 }
 
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerView == fromPickerView ? currencies.count : currenciesExceptBase.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerView == fromPickerView ? currencies[row] : currenciesExceptBase[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let baseCurrencyIndex = self.fromPickerView.selectedRow(inComponent: 0)
+        let toCurrencyIndex = self.toPickerView.selectedRow(inComponent: 0)
+        
+        let baseCurrency = currencies[baseCurrencyIndex]
+        let toCurrency = currenciesExceptBase[toCurrencyIndex]
+        
+        fromCurrencyField.text = baseCurrency
+        toCurrencyField.text = toCurrency
+        activityIndicator.startAnimating()
+        currentRateLabel.isHidden = true
+        
+        API.getCurrencyRate(baseCurrency: baseCurrency, toCurrency: toCurrency) { (value) in
+            DispatchQueue.main.async(execute: {
+                self.activityIndicator.stopAnimating()
+                self.currentRateLabel.isHidden = false
+                
+                self.currentRateLabel.text = "1 \(baseCurrency) = \(value) \(toCurrency)"
+
+                if let doubleValue = Double(value) {
+                    self.currentRate = doubleValue
+                }
+                
+                if let inputValue = Double(self.fromValueField.text!.replacingOccurrences(of: ",", with: ".")) {
+                    self.toValueLabel.text = "\(inputValue * self.currentRate)"
+                }
+            })
+        }
+    }
+}
